974acf92c4c3cf71bc30811abdf03739d129c574
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-04-19 12:06:38 -0300

    Resolução dos exercicios

---
+++ exercicios/para-casa/exercicio1.js
@@ -0,0 +1,9 @@
+/**Exercício 1:**
+**Instruções:** Crie um array com 5 números. Em seguida crie um `for` que imprima cada número do array no console.
+
+_Dica: Utilize a propriedade length do array para definir a condição de parada do loop.*/
+
+const number = [2, 4, 6, 7, 8]
+for (let i = 0; i < number.length; i++) {
+    console.log(number[i])
+}
---
+++ exercicios/para-casa/exercicio4.js
@@ -0,0 +1,15 @@
+/*
+**Exercício 4:**
+
+Instruções:.Crie um array com 5 número.Crie um `while` que imprima cada número do array no console.
+
+_Dica: Utilize uma variável de controle para definir a condição de parada do loop. A cada iteração, incremente essa variável._ 
+*/
+
+const number = [22, 33, 10, 0, 9]
+let i = 0
+while (i <= 4) {
+    console.log(number[i])
+    i++
+}
+
