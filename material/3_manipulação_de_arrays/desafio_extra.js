28dc482a11a7f4cbf0f2a8c1f02ff90d62f438fb
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-04-19 11:44:46 -0300

    all done but exerc 5

--- material/3_manipulação_de_arrays/desafio_extra.js
+++ material/3_manipulação_de_arrays/desafio_extra.js
@@ -1,14 +1,14 @@
 //
 //
 
-const numeros = [10, 23, 45, 67, 89, 12, 34, 56, 78, 90]
+const numeros = [10, 23, 45, 67, 89, 12, 34, 56, 78, 90];
 
-let numerosLivres = numeros
+numeros.pop();
 
-numerosLivres.pop()
+numeros.filter((pares) => pares % 2 == 0);
 
-numerosLivres.forEach(numerosLivres.find((numero) => {
-  if (numero % 2 == 0) {
-    return numero;
-  }
-}))
+let paresDobrados = numeros.map((numero) => {
+  return numero * 2;
+});
+
+console.log(paresDobrados);
