cab323857d8b905761b43a2def345349c7df8b5f
CybelleCouto <cybelinhacouto@hotmail.com>
2023-04-19 18:40:05 -0300

    Atividades da semana 4 feitas menos o  exercicio 5

---
+++ exercicios/para-casa/teste_desafio.js
@@ -0,0 +1,20 @@
+/**let numero = prompt(mensagem = ` Digite um número:`);
+do{
+  console.log("O numero é par");
+}while( numero % 2 == 0)*/
+
+
+
+
+
+/**let readline = require('numero');
+let rl = readline.createInterface(
+    process.stdin, process.srdout)    
+
+    rl.question('Digite um número: ',(numero)=>{
+        do{
+            console.log("O numero é par");
+          }while( numero % 2 == 0)
+        rl.close 
+    })*/
+
