445738c3e41d48ee8787cc9d3bde5994feddda5f
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-04-19 16:21:03 -0300

    trying exerc 5

--- material/1_estruturas_de_repetição/5_exercicio.js
+++ material/1_estruturas_de_repetição/5_exercicio.js
@@ -1,6 +1,19 @@
 //
 //
 
-do { Promise
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-} while ()  
+let numero;
+
+do {
+  readline.question("Digite um número:", (numero) => {
+    if (numero % 2 == 0) {
+      console.log(`${numero} é par!`);
+    } else {
+      console.log(`${numero} é par!`);
+    } readline.close();
+  });
+} while (numero != 0);
