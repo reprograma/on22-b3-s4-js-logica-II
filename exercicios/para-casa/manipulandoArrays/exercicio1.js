43a2e81118f6934ba489a27d0b26970c48d6f276
Jamile <milli.santana2017@gmail.com>
2023-04-19 17:08:57 -0300

    fix: removendo comentários desnecessários

--- exercicios/para-casa/manipulandoArrays/exercicio1.js
+++ exercicios/para-casa/manipulandoArrays/exercicio1.js
@@ -1,13 +1,5 @@
-// Use o método push() para adicionar os números 100 e 200 no final do array.
-// Use o método shift() para remover o primeiro elemento do array.
-// Use o método unshift() para adicionar o número 5 no início do array.
-// Use o método forEach() para imprimir cada número do array no console.
-// Use o método slice() para criar um novo array com os números entre as posições 2 e 5 (inclusive). Imprima o novo array no console.
-// Use o método filter() para criar um novo array com os números maiores que 50. Imprima o novo array no console.
-// Use o método find() para encontrar o primeiro número par do array. Imprima o número encontrado no console.
-// Use o método map() para criar um novo array com o dobro de cada número. Imprima o novo array no console.
-
 const numeros = [10, 23, 45, 67, 89, 12, 34, 56, 78, 90];
+
 // push
 numeros.push(100, 200);
 console.log(numeros);
