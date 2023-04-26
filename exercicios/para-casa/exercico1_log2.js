c4087e51e91b971a248f8ae2d3e57475775b941d
nadiacastilho <ncastilhopersonal@gmail.com>
2023-04-26 18:51:33 +0100

    licao_de_casa_log

---
+++ exercicios/para-casa/exercico1_log2.js
@@ -0,0 +1,8 @@
+
+let numerosDecimais = [10,20,30,40,50]
+
+for (let item of numerosDecimais){
+
+    console.log(item)
+    console.log(numerosDecimais.length)
+}
---
+++ exercicios/para-casa/exercico2_log2.js
@@ -0,0 +1,9 @@
+
+let animal = {
+    nome: "Purê",
+    cor: "preto e marrom",
+    nome_dono: "Nádia Castilho"
+}
+    for (let propriedade in animal){
+        console.log(animal [propriedade])
+    }
